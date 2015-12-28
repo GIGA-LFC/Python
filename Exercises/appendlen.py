688221f3cdd0fa653d04890133a48e24cfe26e34
giga-lfc <gigalfc@gmail.com>
2015-12-28 23:00:07 +0400

    damatebiti leqciebi

---
+++ Exercises/appendlen.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+num=[1,2,3,4,5]
+print len(num) #beWdavs num masivis elementebis raodenobas
+num.append(6) #num masivs daemata erti elementi(cvladi)
+print num,len(num) #davbeWde damatebuli masivis elementebis raodenoba
---
+++ Exercises/index.py~
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+letters=['a','b','c']
+print(letters.index('b')) #a meremdenecaa im mnishvnelobas igebs atvla iwyeba 0 idan
+
+#print(letters.index('z'))
