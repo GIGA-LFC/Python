04adc91801b095a31259586f97e06c7415615a3e
giga-lfc <gigalfc@gmail.com>
2016-04-11 14:07:43 +0400

    daemata callke gaketebuli savarjishoebi

---
+++ Exercises/pythonexcercise/bla.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+file= open("file.txt","w")
+file.write("informaciis damateba")
+file.close()
+
+file=open("file1.txt","r")
+file.read()
+print file.read()
+giga = "gigenco"
+print giga
+str(giga)
+repr(giga)
+print giga
+file.close()
+
+file= open("file.txt","w")
+file.write("sxva informacia")
+file.close()
